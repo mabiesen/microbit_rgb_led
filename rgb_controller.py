81b2cac2d006f35e4483b12f125030d5b813b174
Matthew Biesen <upandcomming88@gmail.com>
2017-05-06 15:54:42 -0400

    Create rgb_controller.py

---
+++ rgb_controller.py
@@ -0,0 +1,21 @@
+# Since the rgb microbit is in the box, lets control it with another microbit
+from microbit import *
+import radio
+
+radio.on()
+
+options = ["flash","change"]
+crnt_option = 0
+
+def change_crnt_option():
+  global crnt_option
+  if crnt_option = (len(options)-1):
+    crnt_option = 0
+  elif:
+    crnt_option += 1
+
+while True:
+  display.scroll(options[crnt_option])
+  if button_a.was_pressed():
+    change_crnt_option()
+  
